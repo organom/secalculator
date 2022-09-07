a54d9960226fb474f25bac64f10c2effcb99bc7b
Ricardo Gomes <ricardo@heptasoft.com>
2022-09-07 21:07:38 +0100

    Rename cubeblocks folder. Adds basic tables to display values

--- src/views/Blocks.tsx
+++ src/views/Blocks.tsx
@@ -2,11 +2,46 @@ import {Container} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import 'rsuite-table/dist/css/rsuite-table.css';
+
 export default function Blocks(props: { blocks: any[] }) {
 	return (
 		<Container>
 			<Link to="/">Back</Link>
 			<div>Total Blocks loaded: {props.blocks.length}</div>
+			<Table virtualized
+				   height={400}
+				   bordered={true}
+				   data={props.blocks}
+				   onRowClick={rowData => {
+					   console.log(rowData);
+				   }}>
+				<Column flexGrow={1} resizable>
+					<HeaderCell>DisplayName</HeaderCell>
+					<Cell dataKey="DisplayName" />
+				</Column>
+				<Column align={'center'} resizable>
+					<HeaderCell>CubeSize</HeaderCell>
+					<Cell dataKey="CubeSize" />
+				</Column>
+				<Column flexGrow={1} resizable>
+					<HeaderCell>Type</HeaderCell>
+					<Cell dataKey="Id.TypeId" />
+				</Column>
+				<Column align={'center'} resizable>
+					<HeaderCell>PCU</HeaderCell>
+					<Cell dataKey="PCU" />
+				</Column>
+				<Column align={'center'} resizable>
+					<HeaderCell>BuildTimeSeconds</HeaderCell>
+					<Cell dataKey="BuildTimeSeconds" />
+				</Column>
+				<Column align={'center'} resizable>
+					<HeaderCell>RequiredPowerInput</HeaderCell>
+					<Cell dataKey="RequiredPowerInput" />
+				</Column>
+			</Table>
 		</Container>
 	);
 }
--- src/views/Components.tsx
+++ src/views/Components.tsx
@@ -2,13 +2,32 @@ import {Container, Stack} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+import 'rsuite-table/dist/css/rsuite-table.css';
+
 export default function Components(props: { components: any[] }) {
 	return (
 		<Container>
 			<Link to="/">Back</Link>
-			<Stack direction="vertical" gap={2}>
-				<div>Total Components loaded: {props.components.length}</div>
-			</Stack>
+			<div>Total Components loaded: {props.components.length}</div>
+			<Table virtualized
+				   height={400}
+				   bordered={true}
+				   data={props.components}
+				   onRowClick={rowData => {
+					   console.log(rowData);
+				   }}>
+				<Column flexGrow={1} resizable>
+					<HeaderCell>Code</HeaderCell>
+					<Cell dataKey="Code" />
+				</Column>
+
+				<Column flexGrow={2} resizable>
+					<HeaderCell>Display Name</HeaderCell>
+					<Cell dataKey="DisplayName" />
+				</Column>
+
+			</Table>
 		</Container>
 	);
 }
