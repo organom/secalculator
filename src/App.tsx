bddece2a66ca681db602a77680c0d4e34c68f5c6
Ricardo Gomes <ricardo@heptasoft.com>
2022-10-17 13:57:48 +0100

    Change way components and cubeblocks are loaded, into a more extensible and generic way. Splig Mods loading code

--- src/App.tsx
+++ src/App.tsx
@@ -7,56 +7,29 @@ import Blocks from './views/Blocks';
 import Components from './views/Components';
 import {parseSBCFile} from './Helpers';
 
-async function downloadAndParseBlockFile(url: string) {
-	const response = await axios.get(url, {
-		responseType: 'blob',
-		headers: {'Content-Type': 'application/octet-stream'}
-	});
-	const blob = new Blob([response.data], {type: response.headers['content-type']});
-	return await parseSBCFile(blob);
-}
-async function parseCubeBlockFile(url: string) {
-	const parsedContent = await downloadAndParseBlockFile(url);
-	return parsedContent.CubeBlocks.Definition || [];
-}
-async function parseComponentFile(url: string) {
-	const parsedContent = await downloadAndParseBlockFile(url);
-	return parsedContent.Components.Component || [];
-}
+const baseComponentsFiles = [ 'Components', 'Components_Economy'];
+const baseCubeBlocksFiles = [ 'CubeBlocks', 'CubeBlocks_Armor', 'CubeBlocks_Armor_2', 'CubeBlocks_Automation', 'CubeBlocks_Communications', 'CubeBlocks_Control', 'CubeBlocks_DecorativePack',
+	'CubeBlocks_DecorativePack2', 'CubeBlocks_Doors', 'CubeBlocks_Economy', 'CubeBlocks_Energy', 'CubeBlocks_Extras', 'CubeBlocks_Frostbite', 'CubeBlocks_Gravity', 'CubeBlocks_Interiors',
+	'CubeBlocks_LCDPanels', 'CubeBlocks_Lights', 'CubeBlocks_Logistics', 'CubeBlocks_Mechanical', 'CubeBlocks_Medical', 'CubeBlocks_Production', 'CubeBlocks_ScrapRacePack',
+	'CubeBlocks_SparksOfTheFuturePack', 'CubeBlocks_Symbols', 'CubeBlocks_Thrusters', 'CubeBlocks_Tools', 'CubeBlocks_Utility', 'CubeBlocks_Warfare1', 'CubeBlocks_Weapons', 'CubeBlocks_Wheels',
+	'CubeBlocks_Windows', 'CubeBlocks_ArmorPanels', 'CubeBlocks_Armor_3', 'CubeBlocks_IndustrialPack', 'CubeBlocks_Warfare2']
 
-async function loadBaseComponents(filesPath: string) {
-	const vanillaFiles = [ 'Components', 'Components_Economy']
-	const promises = vanillaFiles.map(file => parseComponentFile(`${filesPath}/components/${file}.sbc`));
+async function loadSBCFiles(filesPath: string, files: string[]) {
+	async function downloadAndParseBlockFile(url: string) {
+		const response = await axios.get(url, {
+			responseType: 'blob',
+			headers: {'Content-Type': 'application/octet-stream'}
+		});
+		const blob = new Blob([response.data], {type: response.headers['content-type']});
+		const parsedContent = await parseSBCFile(blob);
+		return parsedContent.CubeBlocks.Definition || parsedContent.Components.Component || [];
+	}
+
+	const promises = files.map(file => downloadAndParseBlockFile(`${filesPath}/${file}.sbc`));
 	const results = await Promise.all(promises);
 	return results.flat();
 }
 
-async function loadBaseBlocks(filesPath: string) {
-	const vanillaFiles = [ 'CubeBlocks', 'CubeBlocks_Armor', 'CubeBlocks_Armor_2', 'CubeBlocks_Automation', 'CubeBlocks_Communications', 'CubeBlocks_Control', 'CubeBlocks_DecorativePack',
-		'CubeBlocks_DecorativePack2', 'CubeBlocks_Doors', 'CubeBlocks_Economy', 'CubeBlocks_Energy', 'CubeBlocks_Extras', 'CubeBlocks_Frostbite', 'CubeBlocks_Gravity', 'CubeBlocks_Interiors',
-		'CubeBlocks_LCDPanels', 'CubeBlocks_Lights', 'CubeBlocks_Logistics', 'CubeBlocks_Mechanical', 'CubeBlocks_Medical', 'CubeBlocks_Production', 'CubeBlocks_ScrapRacePack',
-		'CubeBlocks_SparksOfTheFuturePack', 'CubeBlocks_Symbols', 'CubeBlocks_Thrusters', 'CubeBlocks_Tools', 'CubeBlocks_Utility', 'CubeBlocks_Warfare1', 'CubeBlocks_Weapons', 'CubeBlocks_Wheels',
-		'CubeBlocks_Windows', 'CubeBlocks_ArmorPanels', 'CubeBlocks_Armor_3', 'CubeBlocks_IndustrialPack', 'CubeBlocks_Warfare2']
-	const promises = vanillaFiles.map(file => parseCubeBlockFile(`${filesPath}/cubeblocks/Vanilla/${file}.sbc`));
-	const results = await Promise.all(promises);
-	return results.flat();
-}
-
-async function loadSkunkWorksBlocks(filesPath: string) {
-	const skunkWorksFiles = [ 'Aryx_AWE_CubeBlocks', 'CubeBlocks_MorePassages_PassageLux', 'CubeBlocks_Assembler', 'CubeBlocks_OxygenGenerator', 'CubeBlocks_AtmosphericThruster',
-		'CubeBlocks_PassageIntersections_Expansion', 'CubeBlocks_AtmosphericThrusterSciFi', 'CubeBlocks_PassageIntersections_HalfPassageExpansion', 'CubeBlocks_Battery',
-		'CubeBlocks_PassageIntersections_HalfPassages', 'CubeBlocks_Beacon', 'CubeBlocks_PassageIntersections_Lighted', 'CubeBlocks_CargoContainerLarge', 'CubeBlocks_PassageIntersections_LightedHalfPassages',
-		'CubeBlocks_CargoContainerMedium', 'CubeBlocks_PassageIntersections_Passage', 'CubeBlocks_CargoContainerSmall', 'CubeBlocks_Reactor', 'CubeBlocks_Detector', 'CubeBlocks_RebelsGates',
-		'CubeBlocks_DisplayCase', 'CubeBlocks_Refinery', 'CubeBlocks_Drill', 'CubeBlocks_RotaryDoor', 'CubeBlocks_Economy', 'CubeBlocks_SmallConveyors', 'CubeBlocks_Grinder', 'CubeBlocks_SolarPanel',
-		'CubeBlocks_Gyroscope', 'CubeBlocks_Spotlight', 'CubeBlocks_HydrogenEngine', 'CubeBlocks_StoneIncinerator', 'CubeBlocks_HydrogenTank', 'CubeBlocks_Tools', 'CubeBlocks_HydrogenThruster',
-		'CubeBlocks_Tools_ShipGrinder', 'CubeBlocks_IndustrialPack', 'CubeBlocks_Tools_SmallBlockDrill', 'CubeBlocks_IonThruster', 'CubeBlocks_Warfare2_2', 'CubeBlocks_IonThrusterSciFi', 'CubeBlocks_Warfare2',
-		'CubeBlocks_JumpDrive', 'CubeBlocks_Weapons_2', 'CubeBlocks_LargeConveyors', 'CubeBlocks_Weapons', 'CubeBlocks_Logistics', 'CubeBlocks_WindTurbine', 'CubeBlocks_MorePassages_Passage2', 'FASNR_CubeBlocks',
-		'CubeBlocks_MorePassages_Passage3', 'MA_Buster_Cubeblocks', 'CubeBlocks_MorePassages_Passage3Enc', 'MA_HeavyBridge_CubeBlocks', 'CubeBlocks_MorePassages_Passage3EncLight', 'ZardosConnector_CubeBlocks',
-		'CubeBlocks_MorePassages_Passage3EncOffset' ];
-	const promises = skunkWorksFiles.map(file => parseCubeBlockFile(`${filesPath}/cubeblocks/SkunkWorks/${file}.sbc`));
-	const results = await Promise.all(promises);
-	return results.flat();
-}
 const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
 	list.reduce((previous, currentItem) => {
 		const group = getKey(currentItem);
@@ -66,44 +39,52 @@ const groupBy = <T, K extends keyof any>(list: T[], getKey: (item: T) => K) =>
 	}, {} as Record<K, T[]>);
 
 export default function App() {
-	const filesPath: string = 'https://organom.github.io/secalculator';
+	const basePath: string = 'https://organom.github.io/secalculator';
 	const [blocks, setBlocks] = useState<any[]>([]);
 	const [components, setComponents] = useState<any[]>([]);
 	const [loading, setLoading] = useState(true);
 
+	async function loadMod(filesPath: string, componentsFiles: string[], cubeBlocksFiles: string[]) {
+		const loaded = await Promise.all([loadSBCFiles(filesPath, cubeBlocksFiles), loadSBCFiles(filesPath, componentsFiles)]);
+
+		// CubeBlocks
+		const blockIds = new Set(loaded[0].map(d => ({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})));
+		const newBlocks = [...blocks.filter(d => !blockIds.has({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})), ...loaded[0]];
+		setBlocks(newBlocks);
+
+		// Components
+		const compIds = new Set(loaded[1].map(d => ({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})));
+		const newComponents = [...components.filter(d => !compIds.has({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})), ...loaded[1]];
+
+		// load dummy for components used but not defined in the mod
+		const extraComponents = newBlocks.map(x => x.Components.Component).flat();
+		const uniqueExtraComponents = [...new Set(extraComponents.map(t => t['@_Subtype']))];
+		uniqueExtraComponents.forEach(c => {
+			if (!newComponents.some(d => d.Id.SubtypeId === c)) {
+				console.error(`Component ${c} not found, creating dummy`);
+				newComponents.push({Id: {SubtypeId: c, TypeId: 'Component'}});
+			}
+		});
+		setComponents(newComponents);
+	}
+
 	useEffect(() => {
-		loadBaseBlocks(filesPath).then(blocks => {
+		loadSBCFiles(basePath + '/cubeblocks', baseCubeBlocksFiles).then(blocks => {
 			setBlocks(blocks);
-			loadSkunkWorksBlocks(filesPath).then(skunkWorksBlocks => {
-				const ids = new Set(skunkWorksBlocks.map(d => ({SubtypeId: d.Id.SubtypeId, TypeId: d.Id.TypeId})));
-				const newBlocks = [...blocks.filter(d => !ids.has({SubtypeId: d.Id.SubtypeId, TypeId: d.Id.TypeId})), ...skunkWorksBlocks];
+			loadSBCFiles(basePath + '/components', baseComponentsFiles).then(components => {
+				setComponents(components);
 
-				console.log('Blocks');
-				const test = groupBy(newBlocks, (x: any) => x.Id.SubtypeId);
-				for (const property in test) {
-					if(!property) {
-						console.log(test[property])
-						continue;
-					}
-					test[property].length > 1 && console.log(property, test[property].map(d => d.Id.TypeId));
-				}
-
-				setBlocks(newBlocks);
-				loadBaseComponents(filesPath).then(components => {
-					const extraComponents = newBlocks.map(x => x.Components.Component).flat();
-					const uniqueExtraComponents = [...new Set(extraComponents.map(t => t['@_Subtype']))];
-					uniqueExtraComponents.forEach(c => {
-						if (!components.some(d => d.Id.SubtypeId === c)) {
-							console.error(`Component ${c} not found, creating dummy`);
-							components.push({Id: {SubtypeId: c, TypeId: 'Component'}});
-						}
-					});
-					setComponents(components);
+				loadMod(basePath + '/mods/2618476231/Data/Scripts', ['Components'], ['CubeBlocks_Assembler',
+					'CubeBlocks_AtmosphericThruster', 'CubeBlocks_AtmosphericThrusterSciFi', 'CubeBlocks_Battery', 'CubeBlocks_Beacon', 'CubeBlocks_CargoContainerLarge',
+					'CubeBlocks_CargoContainerMedium', 'CubeBlocks_CargoContainerSmall', 'CubeBlocks_Detector',	'CubeBlocks_Drill',	'CubeBlocks_Grinder',
+					'CubeBlocks_Gyroscope',	'CubeBlocks_HydrogenEngine', 'CubeBlocks_HydrogenTank',	'CubeBlocks_HydrogenThruster', 'CubeBlocks_IndustrialPack',
+					'CubeBlocks_IonThruster', 'CubeBlocks_IonThrusterSciFi', 'CubeBlocks_JumpDrive', 'CubeBlocks_OxygenGenerator', 'CubeBlocks_Reactor',
+					'CubeBlocks_Refinery', 'CubeBlocks_SolarPanel', 'CubeBlocks_Spotlight', 'CubeBlocks_StoneIncinerator', 'CubeBlocks_Warfare2', 'CubeBlocks_WindTurbine']).then(() => {
 					setLoading(false);
 				});
 			});
 		});
-	}, [filesPath])
+	}, [basePath])
 
 	return (
 		<div>
