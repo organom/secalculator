c6fc919bf9187efb840fbbe755003b8c185652f8
Ricardo Gomes <ricardo@heptasoft.com>
2022-10-17 19:21:06 +0100

    Adds more mods

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,132 @@ const baseCubeBlocksFiles = [ 'CubeBlocks', 'CubeBlocks_Armor', 'CubeBlocks_Armo
 	'CubeBlocks_LCDPanels', 'CubeBlocks_Lights', 'CubeBlocks_Logistics', 'CubeBlocks_Mechanical', 'CubeBlocks_Medical', 'CubeBlocks_Production', 'CubeBlocks_ScrapRacePack',
 	'CubeBlocks_SparksOfTheFuturePack', 'CubeBlocks_Symbols', 'CubeBlocks_Thrusters', 'CubeBlocks_Tools', 'CubeBlocks_Utility', 'CubeBlocks_Warfare1', 'CubeBlocks_Weapons', 'CubeBlocks_Wheels',
 	'CubeBlocks_Windows', 'CubeBlocks_ArmorPanels', 'CubeBlocks_Armor_3', 'CubeBlocks_IndustrialPack', 'CubeBlocks_Warfare2']
+const skunkWorksModsConfig: { id: string; name: string, cubeBlockFiles: string[]; componentFiles: string[] }[] = [
+	{
+		id: '2618476231',
+		name: 'Skunkworks Tiered Tech',
+		componentFiles: ['Components'],
+		cubeBlockFiles:	['CubeBlocks_Assembler', 'CubeBlocks_AtmosphericThruster', 'CubeBlocks_AtmosphericThrusterSciFi', 'CubeBlocks_Battery', 'CubeBlocks_Beacon',
+			'CubeBlocks_CargoContainerLarge', 'CubeBlocks_CargoContainerMedium', 'CubeBlocks_CargoContainerSmall', 'CubeBlocks_Detector',	'CubeBlocks_Drill',
+			'CubeBlocks_Grinder', 'CubeBlocks_Gyroscope', 'CubeBlocks_HydrogenEngine', 'CubeBlocks_HydrogenTank', 'CubeBlocks_HydrogenThruster', 'CubeBlocks_IndustrialPack',
+			'CubeBlocks_IonThruster', 'CubeBlocks_IonThrusterSciFi', 'CubeBlocks_JumpDrive', 'CubeBlocks_OxygenGenerator', 'CubeBlocks_Reactor',
+			'CubeBlocks_Refinery', 'CubeBlocks_SolarPanel', 'CubeBlocks_Spotlight', 'CubeBlocks_StoneIncinerator', 'CubeBlocks_Warfare2', 'CubeBlocks_WindTurbine']
+	},
+	{
+		id: '1712885149',
+		name: 'Welders+',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_Tools']
+	},
+	{
+		id: '1906186459',
+		name: 'Drills+',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_Tools']
+	},
+	{
+		id: '1712884109',
+		name: 'Grinders+',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_Tools']
+	},
+	{
+		id: '1931509062',
+		name: 'WeaponCore - Replace Vanilla Weapons',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_Weapons']
+	},
+	{
+		id: '2741701803',
+		name: 'Long Range Searchlight',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_Warfare2']
+	},
+	{
+		id: '2015575529',
+		name: 'HangarMarket (Quantum Hangar)',
+		componentFiles: [],
+		cubeBlockFiles:	['MarketProjector']
+	},
+	{
+		id: '2530716039',
+		name: 'Aryx Weapon Enterprises [WeaponCore]',
+		componentFiles: ['Aryx_AWE_Components'],
+		cubeBlockFiles:	['Aryx_AWE_CubeBlocks']
+	},
+	{
+		id: '2704991190',
+		name: 'MorePassages',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_MorePassages_Passage2', 'CubeBlocks_MorePassages_Passage3', 'CubeBlocks_MorePassages_Passage3Enc',
+			'CubeBlocks_MorePassages_Passage3EncLight', 'CubeBlocks_MorePassages_Passage3EncOffset', 'CubeBlocks_MorePassages_PassageLux']
+	},
+	{
+		id: '2783691448',
+		name: 'Zardos Connector Passageway & More',
+		componentFiles: ['ForceField_Components'],
+		cubeBlockFiles:	['ZardosConnector_Connector Junction', 'ZardosConnector_CubeBlocks']
+	},
+	{
+		id: '2771417713',
+		name: 'M&M Experimental',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_Automation']
+	},
+	{
+		id: '1864380341',
+		name: 'AQD - Conveyor Expansion',
+		componentFiles: [],
+		cubeBlockFiles:	['AQD_ConveyorAccess', 'AQD_ConveyorCorner', 'AQD_ConveyorEndcap', 'AQD_ConveyorJunction',
+			'AQD_ConveyorStraight', 'AQD_ConveyorT', 'AQD_ConveyorVent', 'AQD_ConveyorX', 'CubeBlocks_Logistics']
+	},
+	{
+		id: '1359954841',
+		name: 'Rotary Airlock',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_RotaryDoor']
+	},
+	{
+		id: '946724937',
+		name: 'PassageIntersections',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_PassageIntersections_Expansion', 'CubeBlocks_PassageIntersections_HalfPassageExpansion',
+			'CubeBlocks_PassageIntersections_HalfPassages', 'CubeBlocks_PassageIntersections_Lighted',
+			'CubeBlocks_PassageIntersections_LightedHalfPassages', 'CubeBlocks_PassageIntersections_Passage']
+	},
+	{
+		id: '2492169567',
+		name: '[MAD] More Conveyors No Angeled and Block Regroup',
+		componentFiles: [],
+		cubeBlockFiles:	['Large_CubeBlocks', 'Small_CubeBlocks']
+	},
+	{
+		id: '1939935505',
+		name: 'AQD - Armor Expansion',
+		componentFiles: [],
+		cubeBlockFiles:	['AQD_Corner_Split_2x1x1_Base.sbc', 'AQD_Corner_Split_2x1x1_Tip.sbc', 'AQD_Slab_Half_Corner_Split.sbc',
+			'AQD_Slope3x1.sbc', 'AQD_Slope3x1_Corner.sbc', 'AQD_Slope3x1_InvCorner.sbc', 'AQD_Slope4x1.sbc',
+			'AQD_Slope4x1_Corner.sbc', 'AQD_Slope4x1_InvCorner.sbc']
+	},
+	{
+		id: '2799659022',
+		name: 'Display Cases',
+		componentFiles: [],
+		cubeBlockFiles:	['CubeBlocks_DisplayCase']
+	},
+	{
+		id: '2449073590',
+		name: 'MA Buster Blocks',
+		componentFiles: [],
+		cubeBlockFiles:	['MA_Buster_Cubeblocks']
+	},
+	{
+		id: '2433117767',
+		name: 'Interface Block',
+		componentFiles: [],
+		cubeBlockFiles:	['AVTECH_Interface']
+	}
+];
 
 async function loadSBCFiles(filesPath: string, files: string[]) {
 	async function downloadAndParseBlockFile(url: string) {
@@ -31,16 +157,16 @@ async function loadSBCFiles(filesPath: string, files: string[]) {
 }
 
 async function loadMods(components: any[], blocks: any[], filesPath: string, modsConfig: { id: string; name: string, cubeBlockFiles: string[]; componentFiles: string[] }[]) {
-	const loadedBlocks = await Promise.all(modsConfig.map(x => x.cubeBlockFiles?.length > 0 && loadSBCFiles(filesPath + '/' + x.id + '/Data', x.cubeBlockFiles)));
-
 	// CubeBlocks
+	const loadedBlocks = await Promise.all(modsConfig.map(x => x.cubeBlockFiles?.length > 0 ? loadSBCFiles(filesPath + '/' + x.id + '/Data', x.cubeBlockFiles) : undefined).filter(x => x));
 	const cubeBlocks = loadedBlocks.flat();
 	const blockIds = new Set(cubeBlocks.map(d => ({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})));
 	const newBlocks = [...blocks.filter(d => !blockIds.has({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})), ...cubeBlocks];
 
-	const loadedComponents = await Promise.all(modsConfig.map(x => x.componentFiles?.length > 0 && loadSBCFiles(filesPath + '/' + x.id + '/Data', x.componentFiles)));
 	// Components
+	const loadedComponents = await Promise.all(modsConfig.map(x => x.componentFiles?.length > 0 ? loadSBCFiles(filesPath + '/' + x.id + '/Data', x.componentFiles) : undefined).filter(x => x));
 	const componentsBlocks = loadedComponents.flat();
+	console.log(componentsBlocks)
 	const compIds = new Set(componentsBlocks.map(d => ({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})));
 	const newComponents = [...components.filter(d => !compIds.has({TypeId: d.Id.TypeId, SubtypeId: d.Id.SubtypeId})), ...componentsBlocks];
 
@@ -65,47 +191,13 @@ export default function App() {
 
 	useEffect(() => {
 		loadSBCFiles(basePath + '/cubeblocks', baseCubeBlocksFiles).then(blocks => {
-			setBlocks(blocks);
 			loadSBCFiles(basePath + '/components', baseComponentsFiles).then(components => {
-				setComponents(components);
 
-				loadMods(components, blocks, basePath + '/mods',
-					[
-						{
-							id: '2618476231',
-							name: 'Skunkworks Tiered Tech',
-							componentFiles: ['Components'],
-							cubeBlockFiles:	['CubeBlocks_Assembler', 'CubeBlocks_AtmosphericThruster', 'CubeBlocks_AtmosphericThrusterSciFi', 'CubeBlocks_Battery', 'CubeBlocks_Beacon',
-								'CubeBlocks_CargoContainerLarge', 'CubeBlocks_CargoContainerMedium', 'CubeBlocks_CargoContainerSmall', 'CubeBlocks_Detector',	'CubeBlocks_Drill',
-								'CubeBlocks_Grinder', 'CubeBlocks_Gyroscope', 'CubeBlocks_HydrogenEngine', 'CubeBlocks_HydrogenTank', 'CubeBlocks_HydrogenThruster', 'CubeBlocks_IndustrialPack',
-								'CubeBlocks_IonThruster', 'CubeBlocks_IonThrusterSciFi', 'CubeBlocks_JumpDrive', 'CubeBlocks_OxygenGenerator', 'CubeBlocks_Reactor',
-								'CubeBlocks_Refinery', 'CubeBlocks_SolarPanel', 'CubeBlocks_Spotlight', 'CubeBlocks_StoneIncinerator', 'CubeBlocks_Warfare2', 'CubeBlocks_WindTurbine']
-						},
-						{
-							id: '2015575529',
-							name: 'HangarMarket (Quantum Hangar)',
-							componentFiles: [],
-							cubeBlockFiles:	['MarketProjector']
-						},
-						{
-							id: '2530716039',
-							name: 'Aryx Weapon Enterprises [WeaponCore]',
-							componentFiles: ['Aryx_AWE_Components'],
-							cubeBlockFiles:	['Aryx_AWE_CubeBlocks']
-						},
-						{
-							id: '2704991190',
-							name: 'MorePassages',
-							componentFiles: [],
-							cubeBlockFiles:	['CubeBlocks_MorePassages_Passage2', 'CubeBlocks_MorePassages_Passage3', 'CubeBlocks_MorePassages_Passage3Enc',
-								'CubeBlocks_MorePassages_Passage3EncLight', 'CubeBlocks_MorePassages_Passage3EncOffset', 'CubeBlocks_MorePassages_PassageLux']
-						}
-					]).then(result => {
+				loadMods(components, blocks, basePath + '/mods', skunkWorksModsConfig).then(result => {
 					setComponents(result.components);
 					setBlocks(result.blocks);
 					setLoading(false);
 				});
-
 			});
 		});
 	}, [basePath])
